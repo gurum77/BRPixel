748eeffe1fa54bf6d9e11487d2415d720afc5d0f
gurum77 <hileejaeho@gmail.com>
2021-06-18 20:52:18 +0900

    Warning message to work on Invisible layer

--- DrawingArea.gd
+++ DrawingArea.gd
@@ -13,6 +13,15 @@ func _on_DrawingArea_mouse_exited():
 	
 	
 func _gui_input(event):
+	# layer가 꺼져 있으면 클릭 이벤트 안 먹게 한다.
+	if !NodeManager.get_current_layer().visible:
+		if InputManager.is_action_just_released_lbutton(event):
+			Util.show_message(self, "Warning", tr("Layer(%s) is hidden") % [NodeManager.get_current_layer().name], 1)
+			
+		if InputManager.is_action_just_pressed_lbutton(event) || InputManager.is_action_just_pressed_rbutton(event) || InputManager.is_action_just_released_lbutton(event) || InputManager.is_action_pressed_lbutton(event):
+			return
+
+	
 	var nodes = NodeManager.get_tools().get_children()
 	for node in nodes:
 		node.drawing_area_input(event)
--- Tools/MyColorPicker/FirstColorSelectorLine.gd
+++ Tools/MyColorPicker/FirstColorSelectorLine.gd
@@ -6,7 +6,6 @@ onready var last_color_selector_line = get_parent().get_parent().get_node("LastC
 func _draw():
 	var from = Vector2(first_color_selector.selected_x, 0)
 	var to = Vector2(first_color_selector.selected_x, rect_size.y)
-	var selected_color_line = first_color_selector.get_color_on_mouse().blend(Color.red)
 	var draw_color = Color.black
 	draw_line(from, to, draw_color, 1)
 	
