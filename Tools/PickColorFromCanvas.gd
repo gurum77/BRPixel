83f5d3972922db2a7a2c96d9e10bab2cfbc815e2
gurum77 <hileejaeho@gmail.com>
2021-06-18 21:50:41 +0900

    pick color from canvas

--- Tools/PickColorFromCanvas.gd
+++ Tools/PickColorFromCanvas.gd
@@ -13,11 +13,29 @@ func drawing_area_input(event):
 		var pos = get_local_mouse_position()
 		pos.x = floor(pos.x)
 		pos.y = floor(pos.y)
-		NodeManager.get_current_layer().image.lock()
-		var color = NodeManager.get_current_layer().image.get_pixel(pos.x, pos.y)
-		if color.a != 0:
-			StaticData.current_color = color
-		NodeManager.get_current_layer().image.unlock()
 		
+		var find_color = StaticData.current_color
+		var first = true
+		
+		# 모든 layer를 돌면서 색을 찾는다.
+		# visible인 것만 모두 돈다.
+		var layers = NodeManager.get_current_layers().get_normal_layers()		
+		for layer in layers:
+			if !layer.visible:
+				continue
+				
+			layer.image.lock()
+			var color = layer.image.get_pixel(pos.x, pos.y)
+			if color.a == 0:
+				continue
+			if first:
+				find_color = color
+				first = false
+			else:
+				find_color = find_color.blend(color)
+				
+			layer.image.unlock()
+		
+		StaticData.current_color = find_color
 		NodeManager.get_tools().run_last_drawing_tool()
 		
